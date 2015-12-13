13d5bca0dde8f5671c412e2902df5af5df380bc8
Ricardo Ramirez <hola@ricardoerl.com>
2015-12-13 00:12:08 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# arbol-navidad
-Un pequeño experimento en JavaScript para el evento The Hour of Code en El Salvador
+# Árbol de Navidad
+Un pequeño experimento en JavaScript para el evento The Hour of Code en El Salvador.
+
+##Temas abordados
+- HTML y CSS
+- Variables
+- Vectores
+- Funciones
+- Eventos
---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Vector con colores (Tomados de https://gist.github.com/bobspace/2712980)
+var colores = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+// Vector con todos los adornos
+var adornos = document.getElementsByClassName("adorno");
+
+// Asignamos el evento click a cada adorno con la funcion de cambiar color
+for( var i=0; i<adornos.length; i++){
+    adornos[i].addEventListener('click', changeColorBackground, false);
+}
+
+// Funcion para cambiar color
+function changeColorBackground() {
+	var num = getRandomNumber(0, colores.length - 1);
+    this.style.backgroundColor = colores[num];
+};
+
+// Funcion para obtener un numero aleatorio
+function getRandomNumber(min, max) {
+  var offset = min;
+  var range = (max - min) + 1;
+
+  var randomNumber = Math.floor( Math.random() * range) + offset;
+  return randomNumber;
+}
